2f94ff0af25054ea9fecf80e76ad8613b1c270da
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-04-13 00:00:12 -0400

    adicionei extras, e jsons de insert e tabelas

--- bdt1.js
+++ bdt1.js
@@ -34,11 +34,26 @@ class BDT1{
 			queries.push(d[0]);
 			descriptions.push(d[1]);
 		});
+		let tables=JSON.parse(fs.readFileSync("tables.json","utf-8")).data;
+		let insertQueries=JSON.parse(fs.readFileSync("insertQueries.json","utf-8")).data;
 		let css=fs.readFileSync("style.css","utf-8");
 		let header=fs.readFileSync("header.html","utf-8");
 		let footer=fs.readFileSync("footer.html","utf-8");
 		res.write("<style>"+css+"</style>");
 		res.write(header);
+		res.write("<b>Tabelas</b><br>");
+		tables.forEach((elem)=>{
+			try{
+				elem=elem.split("(");
+				elem[0]="<b>"+elem[0]+"</b>";
+				elem=elem.join("(");
+			}catch(e){
+				elem=elem.join("(");
+			}
+			res.write("<span id='querytable'>"+elem+"</span><br>");
+		});
+		res.write("<br><br>");
+		res.write("<span id='title'><b>$n consultas mysql para as tabelas acima</b><br></span>".replace("$n",queries.length));
 		for(let i=0;i<queries.length;i++){
 			let sql=queries[i];
 			let description=descriptions[i];
@@ -51,11 +66,11 @@ class BDT1{
 				rows=await this.query(sql);
 			}catch(e){console.log(e.sqlMessage);}
 			if(rows==null){
-				res.write(this.mat2html([["Erro SQL"]]));
+				res.write(this.mat2html([["Erro SQL"]])+"<br><br>");
 				continue;
 			}
 			if(rows.length==0){
-				res.write(this.mat2html([["Tupla vazia"]]));
+				res.write(this.mat2html([["Tupla vazia"]])+"<br><br>");
 				continue;
 			}
 			let mat=[[]];
@@ -73,6 +88,11 @@ class BDT1{
 			});
 			res.write(this.mat2html(mat)+"<br><br>");
 		}
+		res.write("<br><br>");
+		res.write("<b>Extras - consultas para inserir tuplas nas tabelas:</b><br><br>");
+		insertQueries.forEach((elem)=>{
+			res.write("<span id='insertQuerie'>"+elem+"</span><br>");
+		});
 		res.write(footer);
 		res.end();
 	}
